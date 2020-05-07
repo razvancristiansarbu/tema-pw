9aae1f18ce6f047e9513049b4248a429267a2f1a
razvancristiansarbu <razvan-cristian.sarbu@student.tuiasi.ro>
2020-05-07 17:05:15 +0300

    log in databased

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
-const users = [];
 
 app.set('view engine', 'ejs');
 
@@ -24,29 +24,89 @@ app.get('/signup', (req, res) => {
     res.sendFile(__dirname + "/signup.html");
 });
 
-app.post('/signup', (req, res) => {
-    users.push(req.body);
-    console.log(users);
-    res.redirect('/signin');
-});
-
 app.get('/signin', (req, res) => {
     res.sendFile(__dirname + "/signin.html");
 });
 
-app.post('/signin', (req, res) => {
-    console.log(users);
-    const searchedUser = req.body;
-    console.log(searchedUser);
-    if(users[0].username == searchedUser.username) {
-        console.log("USERNAME GASIT:" + searchedUser.username);
-        if(users[0].password == searchedUser.password) {
-            console.log("PAROLA CORECTA:" + searchedUser.password);
-        }
-    }
-    res.send(`Hello, ${searchedUser.username}`)
+mongoose.connect(('mongodb://localhost:27017/pwhomeworkDB'), {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
 });
 
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', () => {
+    console.log("Connected successfully");
+
+    const userSchema = new mongoose.Schema({
+        firstName: {
+            type: String,
+            required: [true, "User must have a first name."]
+        },
+        lastName: {
+            type: String,
+            required: [true, "User must have a last name."]
+        },
+        username: {
+            type: String,
+            required: [true, "User must have an username."]
+        },
+        email: {
+            type: String,
+            required: [true, "User must have an email."]
+        },
+        password: {
+            type: String,
+            required: [true, "User must have a password."]
+        }
+    });
+
+    const User = mongoose.model('User', userSchema);
+
+    app.post('/signup', (req, res) => {
+        const newUser = new User({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        newUser.save((err, user) => {
+            if(err) {
+                console.err(err);
+            } else {
+                console.log(`${newUser.username} successfully added to the users collection.`);
+            }
+        });
+        
+        res.redirect('/signin');
+    });
+
+
+    app.post('/signin', (req, res) => {
+        const searchedUser = req.body;
+        console.log(searchedUser);
+        User.findOne({
+            username: searchedUser.username
+        }, (err, foundUser) => {
+            if(err) {
+                console.log("Error! Can't find the user.");
+            } else {
+                if(foundUser !== null) {
+                    if(foundUser.password === searchedUser.password) {
+                        console.log(`Logged in: ${foundUser.username}.`);
+                    }
+                } else {
+                    console.log('User not found');
+                }
+            }
+        });
+        res.send(`Hello, ${searchedUser.username}`);
+    });
+});
 
 app.listen(3000, () => {
     console.log("Server running on port 3000.")
